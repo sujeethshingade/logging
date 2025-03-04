02d64751ff1d17995b356d455bfc05f2c946d456
Sujeeth Shingade <sujeethshingade04@gmail.com>
2025-03-04 19:12:16 +0530

    add logging functionality with SLF4J

--- src/main/java/com/logging/logging/LoggingApplication.java
+++ src/main/java/com/logging/logging/LoggingApplication.java
@@ -1,13 +1,18 @@
 package com.logging.logging;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LoggingApplication {
+    
+    private static final Logger logger = LoggerFactory.getLogger(LoggingApplication.class);
 
-	public static void main(String[] args) {
-		SpringApplication.run(LoggingApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        logger.info("Starting Logging Application");
+        SpringApplication.run(LoggingApplication.class, args);
+        logger.info("Logging Application Started Successfully");
+    }
 }
