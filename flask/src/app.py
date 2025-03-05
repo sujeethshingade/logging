abf58c572012efabf64018085696fc20518c9381
Sujeeth Shingade <sujeethshingade04@gmail.com>
2025-03-05 19:47:24 +0530

    add flask server

---
+++ flask/src/app.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from logging.config import dictConfig
+from config.logging_config import LOGGING_CONFIG
+
+app = Flask(__name__)
+dictConfig(LOGGING_CONFIG)
+
+
+@app.route('/')
+def home():
+    app.logger.debug('Debug message')
+    app.logger.info('Info message')
+    app.logger.warning('Warning message')
+    app.logger.error('Error message')
+    app.logger.critical('Critical message')
+    app.logger.exception('Exception message')
+    return "Flask Logging App"
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ flask/src/config/logging_config.py
@@ -0,0 +1,44 @@
+import os
+from logging.handlers import RotatingFileHandler
+import socket
+import getpass
+
+SERVER_NAME = socket.gethostname()
+USER_NAME = getpass.getuser()
+
+LOG_DIR = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'logs')
+if not os.path.exists(LOG_DIR):
+    os.makedirs(LOG_DIR)
+
+LOGGING_CONFIG = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'standard': {
+            'format': '%(asctime)s - Server: ' + SERVER_NAME + ' - User: ' + USER_NAME + ' - %(levelname)s - %(message)s',
+            'datefmt': '%Y-%m-%d %H:%M:%S'
+        },
+    },
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.handlers.RotatingFileHandler',
+            'filename': os.path.join(LOG_DIR, 'application.log'),
+            'maxBytes': 10485760,  # 10MB
+            'backupCount': 5,
+            'formatter': 'standard'
+        },
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'standard'
+        }
+    },
+    'loggers': {
+        '': {  # root logger
+            'handlers': ['console', 'file'],
+            'level': 'DEBUG',
+            'propagate': True
+        }
+    }
+}
